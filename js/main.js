1b5c39235496bd8a8e7c64a60446e60a93427907
LGiki <qq547221432@gmail.com>
2019-04-15 13:23:30 +0800

    1. Support Shanling M0 2. Fix typo in css files

--- js/main.js
+++ js/main.js
@@ -21,13 +21,20 @@ $(function () {
     var URL = window.URL || window.webkitURL;
     var options = {
         aspectRatio: 2 / 3,
-        preview: '.image_preview',
+        preview: '.image_preview_m2x',
         viewMode: 1,
     };
+    var getCroppedCanvasOptions = {
+        width: 320,
+        height: 480,
+        fillColor: '#fff',
+        imageSmoothingEnabled: true,
+        imageSmoothingQuality: 'high',
+    };
     var $image = $('#image');
+    var $inputImage = $('#input_image');
     $image.cropper(options);
     cropper = $image.data('cropper');
-    var $inputImage = $('#input_image');
     $inputImage.change(function () {
         var files = this.files;
         var file;
@@ -47,17 +54,35 @@ $(function () {
             }
         }
     });
+    $('#crop_type').change(function () {
+        let selectCropType = $(this).val();
+        console.log(selectCropType);
+        switch (selectCropType) {
+            case "m0": //M0
+                options.aspectRatio = 1;
+                options.preview = '.image_preview_m0';
+                $('.preview_m2x').toggle();
+                $('.preview_m0').fadeToggle();
+                getCroppedCanvasOptions.width = 240;
+                getCroppedCanvasOptions.height = 240;
+                break;
+            case "m2x": //M2X & M5S
+                options.aspectRatio = 2 / 3;
+                options.preview = '.image_preview_m2x';
+                $('.preview_m2x').fadeToggle();
+                $('.preview_m0').toggle();
+                getCroppedCanvasOptions.width = 320;
+                getCroppedCanvasOptions.height = 480;
+                break;
+            default:
+                break;
+        }
+        $image.cropper('destroy').cropper(options);
+        cropper = $image.data('cropper');
+    });
     $('#crop').click(function () {
-        let resultCanvas = cropper.getCroppedCanvas({
-            width: 320,
-            height: 480,
-            fillColor: '#fff',
-            imageSmoothingEnabled: true,
-            imageSmoothingQuality: 'high',
-        });
-
+        let resultCanvas = cropper.getCroppedCanvas(getCroppedCanvasOptions);
         $('#getCroppedCanvasModal').modal().find('.cropped-image').html(convertCanvasToImage(resultCanvas));
-
     });
 });
 
